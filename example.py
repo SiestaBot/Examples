f4461df89057d04112f1367c72cc0f5ae4f4a4fb
Error44 <hello@error44.tech>
2022-06-11 16:14:54 +0200

    Change API image in json request

--- example.py
+++ example.py
@@ -3,11 +3,11 @@ import aiohttp
 
 async def main():
     async with aiohttp.ClientSession() as session:
-        async with session.get(f"http://api.siesta.red:4444/smile") as r:
-            json = await r.text()
+        async with session.get(f"http://api.siesta.red:4444/smile") as response:
+            json = await response.text()
             print(json)
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(main())
 
->>> r: https://siesta.red/data/smile/smile4.gif
+>>> response: https://siesta.red/data/smile/smile4.gif
