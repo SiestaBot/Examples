59b7d85f8102bddbf37d452d4c08b3da30fef79e
Kimberly <ayla@error44.tech>
2023-12-30 22:20:41 +0100

    Update image

--- image.py
+++ image.py
@@ -3,11 +3,11 @@ import aiohttp
 
 async def main():
     async with aiohttp.ClientSession() as session:
-        async with session.get(f"http://api.siesta.red:4444/smile") as response:
+        async with session.get(f"http://api.siesta.red:4444/smile") as response: # API change in 2024
             image = await response.text()
             print(image)
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(main())
 
->>> response: https://siesta.red/data/smile/smile4.gif
+>>> response: https://siesta.red/data/smile/smile4.gif # link will change in 2024
