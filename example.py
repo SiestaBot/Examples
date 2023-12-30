4b5b124b660ed68f3912a6e0b1197f90a0fc4de5
Kimberly <ayla@error44.tech>
2023-12-30 22:19:41 +0100

    Update example with some infos

--- example.py
+++ example.py
@@ -3,11 +3,11 @@ import aiohttp
 
 async def main():
     async with aiohttp.ClientSession() as session:
-        async with session.get(f"http://api.siesta.red:4444/smile") as response:
+        async with session.get(f"http://api.siesta.red:4444/smile") as response: # API change in 2024
             json = await response.text()
             print(json)
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(main())
 
->>> response: https://siesta.red/data/smile/smile4.gif
+>>> response: https://siesta.red/data/smile/smile4.gif # New link will be added later
